9449e52d34093f80a3d84830ead4274cbeefe26b
afc163 <afc163@gmail.com>
2019-10-12 14:33:09 +0800

    use same home page

--- example/site/pages/index.zh.tsx
+++ example/site/pages/index.zh.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { Skeleton } from 'antd';
 import Layout from '@antv/gatsby-theme-antv/site/components/layout';
 import SEO from '@antv/gatsby-theme-antv/site/components/seo';
+import { useTranslation } from 'react-i18next';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="蚂蚁数据可视化" lang="zh" />
-    <div style={{ margin: '0 auto', padding: '0 80px' }}>
-      中文首页
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-    </div>
-  </Layout>
-);
+const IndexPage = () => {
+  const { t } = useTranslation();
+  return (
+    <Layout>
+      <SEO title="蚂蚁数据可视化" lang="zh" />
+      <div style={{ margin: '0 auto', padding: '0 80px' }}>
+        {t('中文首页')}
+        {t('中文首页1')}
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
