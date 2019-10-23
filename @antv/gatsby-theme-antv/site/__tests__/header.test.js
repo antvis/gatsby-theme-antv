189a674271e056d133e01b3745358fb2696f311a
afc163 <afc163@gmail.com>
2019-10-23 16:47:52 +0800

    fix test case

--- @antv/gatsby-theme-antv/site/__tests__/header.test.js
+++ @antv/gatsby-theme-antv/site/__tests__/header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Header from '../header';
+import Header from '../components/header';
 
 describe(`Header`, () => {
   it(`renders menu`, () => {
