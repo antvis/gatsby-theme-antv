52d8bcb0859bce08dfa7fab2d5669dd8740aeae6
afc163 <afc163@gmail.com>
2019-10-23 21:22:07 +0800

    use built-in locale for theme itself

--- @antv/gatsby-theme-antv/gatsby-ssr.js
+++ @antv/gatsby-theme-antv/gatsby-ssr.js
@@ -8,27 +8,19 @@ const { renderToString } = require('react-dom/server');
 const i18n = require('i18next').default;
 const { initReactI18next } = require('react-i18next');
 const Backend = require('i18next-sync-fs-backend');
-const { getCurrentLangKey } = require('ptz-i18n');
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init();
-
-exports.replaceRenderer = ({
-  bodyComponent,
-  replaceBodyHTMLString,
-  pathname,
-}) => {
-  // const currentLangKey = getCurrentLangKey(['zh', 'en'], 'zh', pathname);
-  i18n.use(Backend).init({
-    initImmediate: false,
-    lng: currentLangKey,
-    backend: {
-      loadPath: 'site/locales/{{lng}}.json',
-    },
-  });
+exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+  i18n
+    .use(initReactI18next)
+    .use(Backend)
+    .init({
+      initImmediate: false,
+      backend: {
+        loadPath: 'site/locales/{{lng}}.json',
+      },
+    });
   // load the common namespace
-  i18n.loadLanguages(['zh', 'en'], () => {
+  i18n.loadLanguages([], () => {
     replaceBodyHTMLString(renderToString(bodyComponent));
   });
 };
