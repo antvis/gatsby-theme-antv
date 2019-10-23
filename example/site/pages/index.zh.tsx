408f016fcb207b725bde2a25aa11461b9dff1a67
afc163 <afc163@gmail.com>
2019-10-23 16:56:48 +0800

    update locale.json

--- example/site/pages/index.zh.tsx
+++ example/site/pages/index.zh.tsx
@@ -4,13 +4,13 @@ import SEO from '@antv/gatsby-theme-antv/site/components/seo';
 import { useTranslation } from 'react-i18next';
 
 const IndexPage = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
     <>
-      <SEO title="蚂蚁数据可视化" lang="zh" />
+      <SEO title={t('蚂蚁数据可视化')} lang={i18n.language} />
       <div style={{ margin: '0 auto', padding: '0 80px' }}>
         {t('中文首页')}
-        {t('中文首页1')}
+        {t('项目里的国际化')}
         <Skeleton />
         <Skeleton />
         <Skeleton />
