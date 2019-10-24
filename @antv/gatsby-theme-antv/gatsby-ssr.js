559667709a174f391fbe4286883a470320c2558b
afc163 <afc163@gmail.com>
2019-10-24 12:23:01 +0800

    debug it

--- @antv/gatsby-theme-antv/gatsby-ssr.js
+++ @antv/gatsby-theme-antv/gatsby-ssr.js
@@ -4,23 +4,19 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
+const i18n = require('./site/i18n').default;
 const { renderToString } = require('react-dom/server');
-const i18n = require('i18next').default;
-const { initReactI18next } = require('react-i18next');
 const Backend = require('i18next-sync-fs-backend');
 
 exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-  i18n
-    .use(initReactI18next)
-    .use(Backend)
-    .init({
-      initImmediate: false,
-      debug: true,
-      backend: {
-        loadPath: `site/locales/{{lng}}.json`,
-      },
-    });
-  i18n.loadLanguages(['zh', 'en'], () => {
+  i18n.use(Backend).init({
+    initImmediate: false,
+    debug: true,
+    backend: {
+      loadPath: `site/locales/{{lng}}.json`,
+    },
+  });
+  i18n.reloadResources(['en', 'zh']).then(() => {
     replaceBodyHTMLString(renderToString(bodyComponent));
   });
 };
--- @antv/gatsby-theme-antv/site/layouts/layout.tsx
+++ @antv/gatsby-theme-antv/site/layouts/layout.tsx
@@ -1,38 +1,13 @@
 import React, { useEffect } from 'react';
 import { useStaticQuery, graphql, withPrefix } from 'gatsby';
 import Footer from 'rc-footer';
+import i18n from '../i18n';
 import { getCurrentLangKey } from 'ptz-i18n';
-import i18n from 'i18next';
-import fetch from 'isomorphic-fetch';
-import Backend from 'i18next-fetch-backend';
-import { initReactI18next, useTranslation } from 'react-i18next';
-import defaultLocale from '../locale.json';
 import Header from '../components/header';
 import footerColumns from '../components/footerColumns';
 import styles from './layout.module.less';
 
 const lngs = ['zh', 'en'];
-
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(Backend)
-  .init({
-    fallbackLng: 'zh',
-    resources: {
-      en: {
-        translation: defaultLocale,
-      },
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}.json',
-      fetch,
-    },
-    react: {
-      useSuspense: false,
-    },
-  });
-
-// i18n.reloadResources(lngs);
 
 interface LayoutProps {
   children: React.ReactElement<any>;
@@ -86,7 +61,6 @@ export default ({
   children: React.ReactElement<any>;
   location: Location;
 }) => {
-  const { i18n } = useTranslation();
   const query = graphql`
     query SiteTitleQuery {
       site {
@@ -110,14 +84,8 @@ export default ({
   const { pathPrefix } = site;
   const path = location.pathname.replace(pathPrefix, '');
   const currentLangKey = getCurrentLangKey(lngs, 'zh', path);
-  console.log(path, currentLangKey);
-  i18n.init({
-    lng: currentLangKey,
-  });
   useEffect(() => {
-    i18n.reloadResources([currentLangKey]).then(() => {
-      i18n.changeLanguage(currentLangKey);
-    });
+    i18n.changeLanguage(currentLangKey);
   }, [currentLangKey]);
   return (
     <Layout siteData={site} location={location}>
