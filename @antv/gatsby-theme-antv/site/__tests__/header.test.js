924912877c2bbb1aa4e94744c8f29d7bde839120
afc163 <afc163@gmail.com>
2019-10-25 16:46:54 +0800

    :white_check_mark: fix ci

--- @antv/gatsby-theme-antv/site/__tests__/header.test.js
+++ @antv/gatsby-theme-antv/site/__tests__/header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Header from '../components/header';
+import Header from '../components/Header';
 
 describe(`Header`, () => {
   it(`renders menu`, () => {
