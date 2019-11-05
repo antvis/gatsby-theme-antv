fc2aa2174b652b03d24c0715f8156628c38e5e22
afc163 <afc163@gmail.com>
2019-11-05 13:59:52 +0800

    Add tooltip for example card

--- @antv/gatsby-theme-antv/site/components/PlayGrounds.tsx
+++ @antv/gatsby-theme-antv/site/components/PlayGrounds.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
+import { Tooltip } from 'antd';
+import { useTranslation } from 'react-i18next';
 import PlayGround, { PlayGroundProps } from './PlayGround';
 import styles from './PlayGrounds.module.less';
 
@@ -12,6 +14,8 @@ const PlayGrounds: React.FC<PlayGroundsProps> = ({
   examples = [],
   location,
 }) => {
+  const { i18n } = useTranslation();
+  console.log(i18n.language);
   const defaultExample =
     examples.find(
       item => `#${item.filename.split('.')[0]}` === location.hash,
@@ -20,29 +24,40 @@ const PlayGrounds: React.FC<PlayGroundsProps> = ({
   return (
     <div className={styles.container}>
       <ul className={styles.cards}>
-        {examples.map(example => (
-          <li
-            key={example.relativePath}
-            onClick={() => {
-              window.location.hash = example.filename.split('.')[0];
-              updateCurrentExample(example);
-            }}
-            role="button"
-            className={classNames(styles.card, {
-              [styles.current]:
-                example.relativePath === currentExample.relativePath,
-            })}
-            title={example.title || currentExample.relativePath}
-          >
-            <img
-              src={
-                example.screenshot ||
-                'https://gw.alipayobjects.com/os/s/prod/antv/assets/image/screenshot-placeholder-b8e70.png'
-              }
-              alt={example.relativePath}
-            />
-          </li>
-        ))}
+        {examples.map(example => {
+          console.log(example.title);
+          const title =
+            typeof example.title === 'object'
+              ? example.title[i18n.language]
+              : example.title;
+          return (
+            <Tooltip title={title || ''} key={example.relativePath}>
+              <li
+                onClick={() => {
+                  history.pushState(
+                    {},
+                    '',
+                    `#${example.filename.split('.')[0]}`,
+                  );
+                  updateCurrentExample(example);
+                }}
+                role="button"
+                className={classNames(styles.card, {
+                  [styles.current]:
+                    example.relativePath === currentExample.relativePath,
+                })}
+              >
+                <img
+                  src={
+                    example.screenshot ||
+                    'https://gw.alipayobjects.com/os/s/prod/antv/assets/image/screenshot-placeholder-b8e70.png'
+                  }
+                  alt={example.relativePath}
+                />
+              </li>
+            </Tooltip>
+          );
+        })}
       </ul>
       <PlayGround
         key={currentExample.relativePath}
