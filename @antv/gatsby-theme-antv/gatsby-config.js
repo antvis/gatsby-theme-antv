99a343d694adde9c58e9d80abc021bfd57c8a482
afc163 <afc163@gmail.com>
2019-11-08 20:38:28 +0800

    remove gatsby-plugin-sitemap

--- @antv/gatsby-theme-antv/gatsby-config.js
+++ @antv/gatsby-theme-antv/gatsby-config.js
@@ -105,7 +105,6 @@ module.exports = ({
       `gatsby-plugin-sharp`,
       `gatsby-transformer-sharp`,
       `gatsby-plugin-offline`,
-      `gatsby-plugin-sitemap`,
       {
         resolve: `gatsby-plugin-manifest`,
         options: {
