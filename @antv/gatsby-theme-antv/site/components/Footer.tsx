3fd0d933779f613c58d84250b3e52682c28b98f8
偏右 <afc163@gmail.com>
2019-11-11 21:09:28 +0800

    feat: Header and Products card (#17)
    * init products card
    
    * import products styles
    
    * tweak menu styles
    
    * Add products data
    
    * Add links
    
    * products card detail
    
    * fix loading page layout
    
    * update products description
    
    * fix z-index

--- @antv/gatsby-theme-antv/site/components/Footer.tsx
+++ @antv/gatsby-theme-antv/site/components/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { default as RCFooter, FooterProps as RcFooterProps } from 'rc-footer';
 import { useTranslation } from 'react-i18next';
 import { Icon } from 'antd';
+import { getProducts } from './getProducts';
 import styles from './Footer.module.less';
 import 'rc-footer/assets/index.less';
 
@@ -19,271 +20,112 @@ const Footer: React.FC<FooterProps> = ({
 }) => {
   const { t, i18n } = useTranslation();
   const lang = language || i18n.language;
-  const defaultColumns = [
-    {
+  const products = getProducts({
+    t,
+    language: lang,
+    rootDomain,
+  });
+
+  const more = {
+    icon: (
+      <img
+        src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg"
+        alt="more products"
+      />
+    ),
+    title: t('更多产品'),
+    items: [
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+            alt="Ant Design"
+          />
+        ),
+        title: 'Ant Design',
+        url: 'https://ant.design',
+        description: t('企业级 UI 设计语言'),
+        openExternal: true,
+      },
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg"
+            alt="yuque"
+          />
+        ),
+        title: t('语雀'),
+        url: 'https://yuque.com',
+        description: t('知识创作与分享工具'),
+        openExternal: true,
+      },
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/rmsportal/uHocHZfNWZOdsRUonZNr.png"
+            alt="yunfengdie"
+          />
+        ),
+        title: t('云凤蝶'),
+        url: 'https://yunfengdie.com',
+        description: t('中台建站平台'),
+        openExternal: true,
+      },
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/antfincdn/v2%24rh7lqpu/82f338dd-b0a6-41bc-9a86-58aaa9df217b.png"
+            alt="Egg"
+          />
+        ),
+        title: 'Egg',
+        url: 'https://eggjs.org',
+        description: t('企业级 Node 开发框架'),
+        openExternal: true,
+      },
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/rmsportal/DMDOlAUhmktLyEODCMBR.ico"
+            alt="kitchen"
+          />
+        ),
+        title: 'Kitchen',
+        description: t('Sketch 工具集'),
+        url: 'https://kitchen.alipay.com',
+        openExternal: true,
+      },
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg"
+            alt="xtech"
+          />
+        ),
+        title: t('蚂蚁体验科技'),
+        url: 'https://xtech.antfin.com/',
+        openExternal: true,
+      },
+    ],
+  };
+
+  const defaultColumns = products
+    .filter(product => product.category !== 'ecology')
+    .map(product => ({
       title: (
         <span>
-          G2<span className={styles.description}>{t('可视化图形语法')}</span>
+          {product.title}
+          <span className={styles.description}>{product.slogan}</span>
         </span>
       ),
-      items: [
-        {
-          title: t('产品首页'),
-          url: `${rootDomain}/g2/${lang}`,
-        },
-        {
-          title: t('图表示例'),
-          url: `${rootDomain}/g2/${lang}/examples`,
-        },
-        {
-          title: t('使用文档'),
-          url: `${rootDomain}/g2/${lang}/docs/manual`,
-        },
-        {
-          title: t('国内镜像'),
-          url: `https://antv.gitee.io/g2`,
-          openExternal: true,
-        },
-      ],
-    },
-    {
-      title: (
-        <span>
-          G6<span className={styles.description}>{t('图可视化引擎')}</span>
-        </span>
-      ),
-      items: [
-        {
-          title: t('产品首页'),
-          url: `${rootDomain}/g6/${lang}`,
-        },
-        {
-          title: t('图表示例'),
-          url: `${rootDomain}/g6/${lang}/examples`,
-        },
-        {
-          title: t('使用文档'),
-          url: `${rootDomain}/g6/${lang}/docs/manual`,
-        },
-        {
-          title: t('国内镜像'),
-          url: `https://antv.gitee.io/g6`,
-          openExternal: true,
-        },
-      ],
-    },
-    {
-      title: (
-        <span>
-          F2<span className={styles.description}>{t('移动可视化方案')}</span>
-        </span>
-      ),
-      items: [
-        {
-          title: t('产品首页'),
-          url: `${rootDomain}/f2/${lang}`,
-        },
-        {
-          title: t('图表示例'),
-          url: `${rootDomain}/f2/${lang}/examples`,
-        },
-        {
-          title: t('使用文档'),
-          url: `${rootDomain}/f2/${lang}/docs/manual`,
-        },
-        {
-          title: t('国内镜像'),
-          url: `https://antv.gitee.io/f2`,
-          openExternal: true,
-        },
-      ],
-    },
-    {
-      title: (
-        <span>
-          L7<span className={styles.description}>{t('地理空间可视化')}</span>
-        </span>
-      ),
-      items: [
-        {
-          title: t('产品首页'),
-          url: '${rootDomain}/l7',
-        },
-        {
-          title: t('图表示例'),
-          url: `${rootDomain}/l7/${lang}/examples`,
-        },
-        {
-          title: t('使用文档'),
-          url: `${rootDomain}/l7/${lang}/docs/manual`,
-        },
-        {
-          title: t('国内镜像'),
-          url: `https://antv.gitee.io/l7`,
-          openExternal: true,
-        },
-      ],
-    },
-    {
-      title: (
-        <span>
-          G2Plot<span className={styles.description}>{t('可视化图表库')}</span>
-        </span>
-      ),
-      items: [
-        {
-          title: t('产品首页'),
-          url: `${rootDomain}/g2plot/${lang}`,
-        },
-        {
-          title: t('图表示例'),
-          url: `${rootDomain}/g2plot/${lang}/examples`,
-        },
-        {
-          title: t('使用文档'),
-          url: `${rootDomain}/g2plot/${lang}/docs/manual`,
-        },
-        {
-          title: t('国内镜像'),
-          url: `https://antv.gitee.io/g2plot`,
-          openExternal: true,
-        },
-      ],
-    },
-    {
-      title: (
-        <span>
-          Graphin<span className={styles.description}>{t('图分析框架')}</span>
-        </span>
-      ),
-      items: [
-        {
-          title: t('产品首页'),
-          url: `${rootDomain}/graphin/${lang}`,
-        },
-        {
-          title: t('图表示例'),
-          url: `${rootDomain}/graphin/${lang}/examples`,
-        },
-        {
-          title: t('使用文档'),
-          url: `${rootDomain}/graphin/${lang}/docs/manual`,
-        },
-        {
-          title: t('国内镜像'),
-          url: `https://antv.gitee.io/graphin`,
-          openExternal: true,
-        },
-      ],
-    },
-    {
-      title: (
-        <span>
-          ChartCube
-          <span className={styles.description}>{t('图表生成工具')}</span>
-        </span>
-      ),
-      items: [
-        {
-          title: t('产品首页'),
-          url: `https://chartcube.alipay.com`,
-          openExternal: true,
-        },
-        {
-          title: t('生成图表'),
-          url: `https://chartcube.alipay.com`,
-          openExternal: true,
-        },
-      ],
-    },
-    {
-      icon: (
-        <img
-          src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg"
-          alt="more products"
-        />
-      ),
-      title: t('更多产品'),
-      items: [
-        {
-          icon: (
-            <img
-              src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-              alt="Ant Design"
-            />
-          ),
-          title: 'Ant Design',
-          url: 'https://ant.design',
-          description: t('企业级 UI 设计语言'),
-          openExternal: true,
-        },
-        {
-          icon: (
-            <img
-              src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg"
-              alt="yuque"
-            />
-          ),
-          title: t('语雀'),
-          url: 'https://yuque.com',
-          description: t('知识创作与分享工具'),
-          openExternal: true,
-        },
-        {
-          icon: (
-            <img
-              src="https://gw.alipayobjects.com/zos/rmsportal/uHocHZfNWZOdsRUonZNr.png"
-              alt="yunfengdie"
-            />
-          ),
-          title: t('云凤蝶'),
-          url: 'https://yunfengdie.com',
-          description: t('中台建站平台'),
-          openExternal: true,
-        },
-        {
-          icon: (
-            <img
-              src="https://gw.alipayobjects.com/zos/antfincdn/v2%24rh7lqpu/82f338dd-b0a6-41bc-9a86-58aaa9df217b.png"
-              alt="Egg"
-            />
-          ),
-          title: 'Egg',
-          url: 'https://eggjs.org',
-          description: t('企业级 Node 开发框架'),
-          openExternal: true,
-        },
-        {
-          icon: (
-            <img
-              src="https://gw.alipayobjects.com/zos/rmsportal/DMDOlAUhmktLyEODCMBR.ico"
-              alt="kitchen"
-            />
-          ),
-          title: 'Kitchen',
-          description: t('Sketch 工具集'),
-          url: 'https://kitchen.alipay.com',
-          openExternal: true,
-        },
-        {
-          icon: (
-            <img
-              src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg"
-              alt="xtech"
-            />
-          ),
-          title: t('蚂蚁体验科技'),
-          url: 'https://xtech.antfin.com/',
-          openExternal: true,
-        },
-      ],
-    },
-  ];
+      items: product.links,
+    }));
+
   return (
     <RCFooter
       maxColumnsPerRow={4}
       theme={theme}
-      columns={columns || defaultColumns}
+      columns={columns || [...defaultColumns, more]}
       className={styles.footer}
       bottom={
         bottom || (
---
+++ @antv/gatsby-theme-antv/site/components/getProducts.ts
@@ -0,0 +1,258 @@
+const tuple = <T extends string[]>(...args: T) => args;
+const Categories = tuple('basic', 'extension', 'ecology');
+
+export interface ProductItem {
+  title: string;
+  icon?: React.ReactNode;
+  slogan?: string;
+  description: string;
+  category: typeof Categories[number];
+  links?: Array<{
+    icon?: React.ReactNode;
+    title: string;
+    url: string;
+    openExternal?: boolean;
+  }>;
+}
+
+export const getProducts = ({
+  t,
+  language,
+  rootDomain = '',
+}: {
+  t: (key: string) => string;
+  language: string;
+  rootDomain?: string;
+}): ProductItem[] => {
+  const products = [
+    {
+      title: 'G2',
+      icon:
+        'https://gw.alipayobjects.com/zos/antfincdn/trEfLRh5pc/G2%252520keshihuatuxingyufa.svg',
+      slogan: t('可视化图形语法'),
+      description: t(
+        'G2 是以数据驱动，具有高度的易用性和扩展性的可视化图形语法。',
+      ),
+      category: Categories[0],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `${rootDomain}/g2/${language}`,
+        },
+        {
+          title: t('图表示例'),
+          url: `${rootDomain}/g2/${language}/examples`,
+        },
+        {
+          title: t('使用文档'),
+          url: `${rootDomain}/g2/${language}/docs/manual`,
+        },
+        {
+          title: t('国内镜像'),
+          url: `https://antv.gitee.io/g2`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'G6',
+      icon:
+        'https://gw.alipayobjects.com/zos/antfincdn/zS1wZZJVcJ/G6%252520tukeshihuayinqing.svg',
+      slogan: t('图可视化引擎'),
+      description: t(
+        '简单、易用、完备的图可视化引擎，帮助开发者搭建图分析应用',
+      ),
+      category: Categories[0],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `${rootDomain}/g6/${language}`,
+        },
+        {
+          title: t('图表示例'),
+          url: `${rootDomain}/g6/${language}/examples`,
+        },
+        {
+          title: t('使用文档'),
+          url: `${rootDomain}/g6/${language}/docs/manual`,
+        },
+        {
+          title: t('国内镜像'),
+          url: `https://antv.gitee.io/g6`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'F2',
+      icon:
+        'https://gw.alipayobjects.com/zos/antfincdn/D%26fDbWqVkv/F2%252520yidongduankeshihuafangan.svg',
+      slogan: t('移动可视化方案'),
+      description: t(
+        '专注于移动端的可视化解决方案，兼容 H5/Node/小程序/Weex 等多端环境',
+      ),
+      category: Categories[0],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `${rootDomain}/f2/${language}`,
+        },
+        {
+          title: t('图表示例'),
+          url: `${rootDomain}/f2/${language}/examples`,
+        },
+        {
+          title: t('使用文档'),
+          url: `${rootDomain}/f2/${language}/docs/manual`,
+        },
+        {
+          title: t('国内镜像'),
+          url: `https://antv.gitee.io/f2`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'L7',
+      icon:
+        'https://gw.alipayobjects.com/zos/antfincdn/OI%26h7HXH33/L7%252520dilikongjianshujukeshihua.svg',
+      slogan: t('地理空间数据可视化'),
+      description: t('易扩展，高性能，安全可靠的地理空间数据可视化解决方案。'),
+      category: Categories[0],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `${rootDomain}/l7/${language}`,
+        },
+        {
+          title: t('图表示例'),
+          url: `${rootDomain}/l7/${language}/examples`,
+        },
+        {
+          title: t('使用文档'),
+          url: `${rootDomain}/l7/${language}/docs/manual`,
+        },
+        {
+          title: t('国内镜像'),
+          url: `https://antv.gitee.io/l7`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'G2Plot',
+      icon: 'https://gw.alipayobjects.com/zos/antfincdn/SlbIagEvT7/G2plot.svg',
+      slogan: t('强大的图表库'),
+      description: t('G2Plot 是一套在图形语法基础上封装的常用图表库。'),
+      category: Categories[1],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `${rootDomain}/g2plot/${language}`,
+        },
+        {
+          title: t('图表示例'),
+          url: `${rootDomain}/g2plot/${language}/examples`,
+        },
+        {
+          title: t('使用文档'),
+          url: `${rootDomain}/g2plot/${language}/docs/manual`,
+        },
+        {
+          title: t('国内镜像'),
+          url: `https://antv.gitee.io/g2plot`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'Graphin',
+      icon: 'https://gw.alipayobjects.com/zos/antfincdn/0b4HzOcEJY/Graphin.svg',
+      slogan: t('图分析框架'),
+      description: t('开箱即用的图分析应用 React 组件库'),
+      category: Categories[1],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `${rootDomain}/graphin/${language}`,
+        },
+        {
+          title: t('图表示例'),
+          url: `${rootDomain}/graphin/${language}/examples`,
+        },
+        {
+          title: t('使用文档'),
+          url: `${rootDomain}/graphin/${language}/docs/manual`,
+        },
+        {
+          title: t('国内镜像'),
+          url: `https://antv.gitee.io/graphin`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'ChartCube',
+      icon:
+        'https://gw.alipayobjects.com/zos/antfincdn/kM2Rd6CnlW/ChartCube.svg',
+      slogan: t('图表魔方'),
+      description: t('AntV 在线图表制作利器。'),
+      category: Categories[1],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `https://chartcube.alipay.com`,
+          openExternal: true,
+        },
+        {
+          title: t('生成图表'),
+          url: `https://chartcube.alipay.com/guide`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: t('墨者学院'),
+      icon:
+        'https://gw.alipayobjects.com/zos/antfincdn/12j36RPVldO/mozhexueyuan.svg',
+      description: t('数据可视化社区'),
+      category: Categories[2],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `https://www.yuque.com/mo-college`,
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'BizCharts',
+      icon:
+        'https://gw.alipayobjects.com/zos/antfincdn/Q1pbg%26O2TM/BizCharts.svg',
+      description: t('基于商业场景下的数据可视化解决方案'),
+      category: Categories[2],
+      links: [
+        {
+          title: t('产品首页'),
+          url: 'https://bizcharts.net',
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      title: 'Viser',
+      icon: 'https://gw.alipayobjects.com/zos/antfincdn/QAyW5h99HVa/Viser.svg',
+      description: t('基于 G2 实现的可视化解决方案'),
+      category: Categories[2],
+      links: [
+        {
+          title: t('产品首页'),
+          url: `https://viserjs.github.io/`,
+          openExternal: true,
+        },
+      ],
+    },
+  ];
+
+  return products;
+};
