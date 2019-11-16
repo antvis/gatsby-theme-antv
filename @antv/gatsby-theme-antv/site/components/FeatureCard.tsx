e9519f09f35dd9fe9291d3f44684d2a170485fcd
Yanyan-Wang <yanyanwang93@gmail.com>
2019-11-16 21:20:30 +0800

    fix: feature card divider.

--- @antv/gatsby-theme-antv/site/components/FeatureCard.tsx
+++ @antv/gatsby-theme-antv/site/components/FeatureCard.tsx
@@ -5,12 +5,24 @@ interface FeatureProps {
   icon: string;
   title: string;
   description: string;
+  divider: boolean;
 }
 
-const FeatureCard: React.FC<FeatureProps> = ({ icon, title, description }) => {
+const FeatureCard: React.FC<FeatureProps> = ({
+  icon,
+  title,
+  description,
+  divider = false,
+}) => {
   return (
     <div className={styles.card}>
-      <div className={styles.content}>
+      <div
+        className={styles.content}
+        style={{
+          borderRight: divider ? '1px solid rgba(0, 0, 0, 0.1)' : '0px',
+          borderLeft: divider ? '1px solid rgba(0, 0, 0, 0.1)' : '0px',
+        }}
+      >
         <img className={styles.icon} src={icon} alt="advantage" />
         <p className={styles.title}>{title}</p>
         <p className={styles.description}>{description}</p>
