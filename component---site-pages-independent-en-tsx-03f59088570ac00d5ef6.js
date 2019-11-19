d538d86b3c7abe295ad10d2c78da7bc73e8645fc
afc163 <afc163@gmail.com>
2019-11-19 19:36:18 +0800

    Updates

---
+++ component---site-pages-independent-en-tsx-03f59088570ac00d5ef6.js
@@ -0,0 +1,38 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[7,8],{
+
+/***/ "BxFN":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("mXGw");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_i18next__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("CE6G");
+
+
+
+var Indepent = function Indepent() {
+  var _useTranslation = Object(react_i18next__WEBPACK_IMPORTED_MODULE_1__[/* useTranslation */ "b"])(),
+      t = _useTranslation.t;
+
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, t('独立页面'));
+};
+
+Indepent.noLayout = true;
+/* harmony default export */ __webpack_exports__["default"] = (Indepent);
+
+/***/ }),
+
+/***/ "tOJG":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _independent_zh__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("BxFN");
+
+/* harmony default export */ __webpack_exports__["default"] = (_independent_zh__WEBPACK_IMPORTED_MODULE_0__["default"]);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=component---site-pages-independent-en-tsx-03f59088570ac00d5ef6.js.map
