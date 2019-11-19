55f43b8feec2b8f6d1b4cf6776b68af423d91cde
afc163 <afc163@gmail.com>
2019-11-19 18:59:14 +0800

    Updates

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4b7055c11755c33a5fdd862f51072c55"
+    "revision": "cf856be5ae48c71a18ce77f0646c6791"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -144,7 +144,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/gatsby-theme-antv/app-a939d5dcfab0e580fbf0.js`))) {
+  if (!resources || !(await caches.match(`/gatsby-theme-antv/app-45af906ac3e07da88696.js`))) {
     return await fetch(event.request)
   }
 
