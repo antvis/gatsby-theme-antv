1271d968750c571820a442d9599bd77ddb4787fa
afc163 <afc163@gmail.com>
2019-11-25 16:10:32 +0800

    fix reading time always one minute
    close #62

--- @antv/gatsby-theme-antv/site/components/ReadingTime.tsx
+++ @antv/gatsby-theme-antv/site/components/ReadingTime.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Tag } from 'antd';
-import moment from 'moment';
 import { useTranslation } from 'react-i18next';
 
 const ReadingTime: React.FC<any> = ({ readingTime }) => {
   const { i18n } = useTranslation();
-  if (!readingTime.time) {
-    return <Tag>{readingTime.text}</Tag>;
-  }
   return (
     <Tag>
       {i18n.language === 'zh'
-        ? moment(readingTime.time).format('阅读时间约 M 分钟')
+        ? readingTime.text.replace(
+            /(\d)\smin\sread/,
+            (_: string, min: string) => {
+              if (min) {
+                return `阅读时间约 ${min} 分钟`;
+              }
+              return `阅读时间约 ${Math.ceil(readingTime.time / 60000)} 分钟`;
+            },
+          )
         : readingTime.text}
     </Tag>
   );
