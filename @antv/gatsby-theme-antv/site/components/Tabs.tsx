c3874a5b5f2dc8c438f05b13b130666df9c3101e
afc163 <afc163@gmail.com>
2019-12-12 11:51:38 +0800

    use H2 tag in example tabs

--- @antv/gatsby-theme-antv/site/components/Tabs.tsx
+++ @antv/gatsby-theme-antv/site/components/Tabs.tsx
@@ -25,7 +25,9 @@ const Tabs: React.FC<{
           [styles.hidden]: showTabs.examples === false,
         })}
       >
-        <Link to={slug}>{t('代码演示')}</Link>
+        <Link to={slug}>
+          <h2>{t('代码演示')}</h2>
+        </Link>
       </li>
       <li
         className={classNames({
@@ -33,7 +35,9 @@ const Tabs: React.FC<{
           [styles.hidden]: showTabs.API === false,
         })}
       >
-        <Link to={`${slug}/API`}>API</Link>
+        <Link to={`${slug}/API`}>
+          <h2>API</h2>
+        </Link>
       </li>
       <li
         className={classNames({
@@ -41,7 +45,9 @@ const Tabs: React.FC<{
           [styles.hidden]: showTabs.design === false,
         })}
       >
-        <Link to={`${slug}/design`}>{t('设计指引')}</Link>
+        <Link to={`${slug}/design`}>
+          <h2>{t('设计指引')}</h2>
+        </Link>
       </li>
     </ul>
   );
