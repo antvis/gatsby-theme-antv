380bc48c064e211ca8b85f41b50faee9bfa0bc67
afc163 <afc163@gmail.com>
2020-01-07 15:19:36 +0800

    fix unexpected scroll between example tabs
    https://github.com/gatsbyjs/gatsby/issues/11898#issuecomment-465706313

--- example/gatsby-browser.js
+++ example/gatsby-browser.js
@@ -1,1 +1,30 @@
 window.g2 = require('./src/index.ts').default;
+
+// gatsby-browser.js
+exports.shouldUpdateScroll = ({ routerProps, prevRouterProps }) => {
+  if (routerProps.location.hash) {
+    return false;
+  }
+  const { pathname: prevPathname } = prevRouterProps.location;
+  const { pathname: currentPathname } = routerProps.location;
+  // 演示里的 Tabs 切换，不进行页面滚动
+  if (
+    prevPathname.includes('/examples/') &&
+    currentPathname.includes('/examples/')
+  ) {
+    const prevPathes = prevPathname.split('/');
+    const currentPathes = currentPathname.split('/');
+    if (prevPathes.length === currentPathes.length) {
+      return (
+        prevPathes.slice(0, -1).join('/') !==
+        currentPathes.slice(0, -1).join('/')
+      );
+    }
+    const compareLength = Math.min(prevPathes.length, currentPathes.length);
+    return (
+      prevPathes.slice(0, compareLength).join('/') !==
+      currentPathes.slice(0, compareLength).join('/')
+    );
+  }
+  return true;
+};
