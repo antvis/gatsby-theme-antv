b926750d5608ddd7baf80e7fb369da3cff7e76bb
afc163 <afc163@gmail.com>
2020-01-15 15:14:57 +0800

    :zap: refactor prev and next navigator

--- @antv/gatsby-theme-antv/site/hooks.ts
+++ @antv/gatsby-theme-antv/site/hooks.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { NavigatorBannerProps } from './components/NavigatorBanner';
 
 export const useChinaMirrorHost = () => {
   const [isChinaMirrorHost, setIsChinaMirrorHost] = useState(false);
@@ -11,4 +12,39 @@ export const useChinaMirrorHost = () => {
     }
   }, []);
   return [isChinaMirrorHost];
+};
+
+export const usePrevAndNext = () => {
+  const [prevAndNext, setPrevAndNext] = useState<
+    NavigatorBannerProps['post'][]
+  >([]);
+  useEffect(() => {
+    const menuNodes = document.querySelectorAll('aside .ant-menu-item');
+    const currentMenuNode = document.querySelector(
+      'aside .ant-menu-item-selected',
+    );
+    const currentIndex = Array.from(menuNodes).findIndex(
+      node => node === currentMenuNode,
+    );
+    const prevNode =
+      currentIndex - 1 >= 0 ? menuNodes[currentIndex - 1] : undefined;
+    const nextNode =
+      currentIndex + 1 < menuNodes.length
+        ? menuNodes[currentIndex + 1]
+        : undefined;
+    const prev = prevNode
+      ? {
+          slug: prevNode.querySelector('a')?.getAttribute('href') || undefined,
+          title: prevNode.querySelector('a')?.innerText,
+        }
+      : undefined;
+    const next = nextNode
+      ? {
+          slug: nextNode.querySelector('a')?.getAttribute('href') || undefined,
+          title: nextNode.querySelector('a')?.innerText,
+        }
+      : undefined;
+    setPrevAndNext([prev, next]);
+  }, []);
+  return prevAndNext;
 };
