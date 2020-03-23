a2b12c6b30e336e4a794ba4fc78ee56e7e0c9b96
afc163 <afc163@gmail.com>
2020-03-23 16:25:51 +0800

    initial css of Swatch

--- @antv/gatsby-theme-antv/site/components/Swatch.tsx
+++ @antv/gatsby-theme-antv/site/components/Swatch.tsx
@@ -1,3 +1,34 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Switch } from 'antd';
+import styles from './Swatch.module.less';
 
-export default () => <div>swatch 1</div>;
+interface SwatchProps {
+  title: string;
+  darkmode?: boolean;
+  colors?: string[];
+}
+
+const Swatch: FC<SwatchProps> = ({ title, darkmode, colors = [] }) => {
+  return (
+    <div className={styles.swatch}>
+      {title && (
+        <div className={styles.heading}>
+          <h3>{title}</h3>
+          {darkmode && (
+            <div>
+              Dark mode: <Switch />
+            </div>
+          )}
+        </div>
+      )}
+      <div className={styles.colors}>
+        色彩：
+        {colors.map(color => (
+          <span key={color}>{color}</span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Swatch;
--- example/docs/specification/getting-started.zh.md
+++ example/docs/specification/getting-started.zh.md
@@ -14,7 +14,7 @@ redirect_from:
 
 G2 是一套基于可视化编码的图形语法，以数据驱动，具有高度的易用性和扩展性，用户无需关注各种繁琐的实现细节，一条语句即可构建出各种各样的可交互的统计图表。
 
-<swatch></swatch>
+<swatch title="色板"></swatch>
 
 **在此衷心感谢[《The Grammar of Graphics》](https://www.cs.uic.edu/~wilkinson/TheGrammarOfGraphics/GOG.html)的作者 [Leland Wilkinson](https://en.wikipedia.org/wiki/Leland_Wilkinson)，为 G2 的图形语法提供了理论基础！**
 
