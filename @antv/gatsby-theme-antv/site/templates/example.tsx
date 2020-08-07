9ca40d7b9a9187af920e2ba62dac73cae621dbbe
afc163 <afc163@gmail.com>
2020-08-07 18:31:09 +0800

    fix: hide content of inactive tabs

--- @antv/gatsby-theme-antv/site/templates/example.tsx
+++ @antv/gatsby-theme-antv/site/templates/example.tsx
@@ -386,33 +386,29 @@ export default function Template({
         }}
         examplesCount={(exampleSections.examples || []).length}
       />
-      {exampleSections.examples && (
-        <div style={{ display: activeTab === 'examples' ? 'block' : 'none' }}>
-          <PlayGrounds
-            examples={exampleSections.examples}
-            location={location}
-            playground={playground || {}}
-          />
-        </div>
-      )}
-      {exampleSections.API && (
+      {exampleSections.examples && activeTab === 'examples' ? (
+        <PlayGrounds
+          examples={exampleSections.examples}
+          location={location}
+          playground={playground || {}}
+        />
+      ) : null}
+      {exampleSections.API && activeTab === 'API' ? (
         <div
-          style={{ display: activeTab === 'API' ? 'block' : 'none' }}
           /* eslint-disable-next-line react/no-danger */
           dangerouslySetInnerHTML={{
             __html: exampleSections.API.node.html,
           }}
         />
-      )}
-      {exampleSections.design && (
+      ) : null}
+      {exampleSections.design && activeTab === 'design' ? (
         <div
-          style={{ display: activeTab === 'design' ? 'block' : 'none' }}
           /* eslint-disable-next-line react/no-danger */
           dangerouslySetInnerHTML={{
             __html: exampleSections.design.node.html,
           }}
         />
-      )}
+      ) : null}
     </>
   );
 
