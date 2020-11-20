8706e7357bc14d3fc59c066ae6c1b31180579b7b
yingying.xwy <yingying.xwy@antgroup.com>
2020-11-21 00:44:52 +0800

    feat: update version of g2 && change the themecode for g2 and g2plot

--- example/examples/basic/demo/ts-demo.ts
+++ example/examples/basic/demo/ts-demo.ts
@@ -1,14 +1,50 @@
 import { Chart } from '@antv/g2';
 
-fetch('data/fireworks-sales.json')
-  .then((response) => response.json())
-  .then((data) => {
-    const chart = new Chart({
-      container: document.getElementById('container') as HTMLDivElement,
-      autoFit: true,
-    });
+const data = [
+  { time: '9:00-10:00', value: 30 },
+  { time: '10:00-11:00', value: 90 },
+  { time: '11:00-12:00', value: 50 },
+  { time: '12:00-13:00', value: 30 },
+  { time: '13:00-14:00', value: 70 },
+];
 
-    chart.source(data);
-    chart.interval().position('scales*Date').color('scales');
-    chart.render();
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+  height: 500,
+});
+chart.data(data);
+chart.scale('value', {
+  alias: '销售额(万)',
+  nice: true,
+});
+chart.axis('time', {
+  tickLine: null,
+});
+
+chart.tooltip({
+  showMarkers: false,
+});
+chart.interaction('active-region');
+
+chart
+  .interval()
+  .position('time*value')
+  .style('time', (val) => {
+    if (val === '13:00-14:00') {
+      return {
+        fillOpacity: 0.4,
+        lineWidth: 1,
+        stroke: '#636363',
+        lineDash: [3, 2],
+      };
+    }
+    return {
+      fillOpacity: 1,
+      lineWidth: 0,
+      stroke: '#636363',
+      lineDash: [3, 2],
+    };
   });
+
+chart.render();
