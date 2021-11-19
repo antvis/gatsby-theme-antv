2cdbcb431713859872c993ac60647ff7a0ba5270
visiky <xinming.lxj@antgroup.com>
2021-11-19 08:53:04 +0800

    fix: 修复 jest 使用 fetch 问题

--- @antv/gatsby-theme-antv/jest.setup.js
+++ @antv/gatsby-theme-antv/jest.setup.js
@@ -1,3 +1,5 @@
 require('@testing-library/jest-dom/extend-expect');
 require('matchmedia-polyfill');
 require('matchmedia-polyfill/matchMedia.addListener');
+
+global.fetch = jest.fn(() => Promise.resolve({}));
